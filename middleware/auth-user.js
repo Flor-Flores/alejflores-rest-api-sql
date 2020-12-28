33622ce4280bc9566e56dcf1244e83b99dc9a803
alejflores <2alejflores2@gmail.com>
2020-12-28 16:59:24 -0600

    User Routes Created

--- middleware/auth-user.js
+++ middleware/auth-user.js
@@ -10,37 +10,38 @@ exports.authenticateUser = async (req, res, next) => {
 
   // Parse the user's credentials from the Authorization header.
   const credentials = auth(req);
-
+  console.log(credentials)
   // If the user's credentials are available...
-     // Attempt to retrieve the user from the data store
-     // by their username (i.e. the user's "key"
-     // from the Authorization header).
-     if (credentials) {
-      const user = await User.findOne({ where: {username: credentials.name} });
-      if (user) {
-        const authenticated = bcrypt
-          .compareSync(credentials.pass, user.confirmedPassword);
-        if (authenticated) {
-          console.log(`Authentication successful for username: ${user.username}`);
-  
-          // Store the user on the Request object.
-          req.currentUser = user;
-        } else {
-          message = `Authentication failure for username: ${user.username}`;
-        }
-      } else {
-        message = `User not found for username: ${credentials.name}`;
-      }
-    } else {
-      message = 'Auth header not found';
-    }
+    // Attempt to retrieve the user from the data store
+    // by their username (i.e. the user's "key"
+    // from the Authorization header).
+    if (credentials) {
+    const user = await User.findOne({ where: {emailAddress: credentials.name} });
 
-    if (message) {
-      console.log(message);
-      res.status(401).json({ message: 'Access Denied'});
-    } else {
-      next();
-    }
-  
+    if (user) {
+      console.log(user);
+      const authenticated = bcrypt
+        .compareSync(credentials.pass, user.password);
+      if (authenticated) {
+        console.log(`Authentication successful for username: ${user.firstName + ' ' + user.lastName}`);
+
+        // Store the user on the Request object.
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for username: ${user.firstName}`;
+      }
+      } else {
+        message = `User not found for username: ${credentials.firstName}`;
+      }
+      } else {
+        console.log(" auth header   not     found")
+        message = 'Auth header not found';
+      }
+
+  if (message) {
+    console.warn(message);
+    res.status(401).json({ message: 'Access Denied' });
+  } else {
     next();
-  };
+  }
+};
--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const { asyncHandler } = require('./middleware/async-handler');
 const { User, Course } = require('./models');
-// const { authenticateUser } = require('./middleware/auth-user');
+const { authenticateUser } = require('./middleware/auth-user');
 
 // Construct a router instance.
 const router = express.Router();
@@ -18,13 +18,37 @@ const router = express.Router();
 // Route that returns a list of users.
 
 // Route that returns a list of users.
-router.get('/users', async (req, res) => {
+router.get('/users', authenticateUser, asyncHandler(async (req, res) => {
+  const user = req.currentUser;
 
-  res.json({ 
-      "test": "ok"
+
+  try {
+    console.log("        im at               23" ,   user , "  end of 26")
+  res.json({
+    name: user.firstName,
+    username: user.emailAddress
   });
 
-});
+
+
+    res.status(201).end();
+  } catch (error) {
+      console.log(" we have an error   at  29 routes")
+    if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+      const errors = error.errors.map(err => err.message);
+      res.status(400).json({ errors });   
+    } else {
+      throw error;
+    }
+  }
+
+
+  // res.json({
+  //   name: user.firstName,
+  //   username: user.emailAddress
+  // });
+
+}));
 
 
 // Route that creates a new user.
@@ -33,23 +57,13 @@ router.post('/users', asyncHandler(async (req, res) => {
 
 
     console.log(user + " this is hell");
-    res.status(201).json({ "message": "Account successfully created!" }).end();
-
+    // res.status(201).json({ "message": "Account successfully created!" })
+    // // .end();
 
 
 
   try {
     console.log(req.body);
-    // const jane = await User.create({ 
-    //   firstName: "John",
-    //   lastName: "Smith",
-    //   emailAddress: "ooohssssdddn@smith.com",
-    //   password: "password"
-  
-    // });
-    // console.log(req.body);
-
-
     await User.create(req.body);
     res.status(201).json({ "message": "Account successfully created!" });
   } catch (error) {
