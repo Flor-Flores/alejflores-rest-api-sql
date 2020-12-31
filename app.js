64200f03245a8fe632a83ccd75df723bbe244ad2
alejflores <2alejflores2@gmail.com>
2020-12-31 10:29:28 -0600

    close to exceeds expectations

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ const {Sequelize} = require('sequelize');
 
 const routes = require('./routes');
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'path/to/database.sqlite'
-});
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
@@ -19,7 +15,7 @@ const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'tr
 const app = express();
 
 app.use(express.json());
-
+  
 
 // setup morgan which gives us http request logging
 app.use(morgan('dev'));
